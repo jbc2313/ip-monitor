52d3e305f18f7833e517f6386bd3c890b8ecedce
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-01-07 15:23:55 -0600

    added epoch time tracker

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,7 @@
     windows_subsystem = "windows"
 )]
 
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,9 +12,15 @@ fn greet(name: &str) -> String {
 
 #[tauri::command]
 fn time() -> String {
-    use std::time::SystemTime;
-    let cur_time = SystemTime::now();
-    format!("Hello World, The current time is {:?}", cur_time)
+    use std::time::{SystemTime, UNIX_EPOCH};
+    let cur_time_sec: i64 = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs() as i64;
+    let days_past = &cur_time_sec/86400;
+    let years_past: f64 = days_past as f64/365.25;
+    //cur_time in sec from epoch
+    format!("Hello World, The current time is {:?} seconds past EPOCH, days passed is {:?}, years passed is {:?}", cur_time_sec, days_past, years_past)
 }
 
 fn main() {
